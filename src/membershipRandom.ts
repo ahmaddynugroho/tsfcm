7fd66fcbf8d506829321a4c56f5f65dba2a0a231
Ahmad D. Y. Nugroho <ahmaddwi700@gmail.com>
2022-05-24 21:52:04 +0700

    feat: change U random algorithm

--- src/membershipRandom.ts
+++ src/membershipRandom.ts
@@ -2,20 +2,14 @@ export function membershipRandom(c: number, X: number[][]): number[][] {
     let U: number[][] = [...Array(X.length)].map(() => [])
 
     for (let i = 0; i < U.length; i++) {
-        const data = U[i]
+        let data = U[i]
 
-        let pivot: number[] = []
-        for (let pivotI = 0; pivotI < c - 1; pivotI++) {
-            pivot.push(Math.floor(Math.random() * 100))
+        for (let k = 0; k < c; k++) {
+            data.push(Math.floor(Math.random() * 100))
         }
-        pivot.sort((a, b) => a - b)
-        pivot.push(100)
 
-        for (let pivotI = 0; pivotI < pivot.length; pivotI++) {
-            const p = pivot[pivotI]
-            const p0 = pivotI === 0 ? 0 : pivot[pivotI - 1]
-            data.push((p - p0) / 100)
-        }
+        const denominator = data.reduce((p, c) => p + c)
+        data = data.map((el) => el / denominator)
     }
 
     return U
