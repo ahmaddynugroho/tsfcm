a1f12ee50a81a384668f01fe6b88870f9d15323d
Ahmad D. Y. Nugroho <ahmaddwi700@gmail.com>
2022-05-24 20:35:46 +0700

    chore: dead code / comment

--- src/main.ts
+++ src/main.ts
@@ -10,21 +10,18 @@ function fcm(
     err: number,
     X: number[][]
 ): number[][] {
-    let U: number[][] = membershipRandom(c, X)
+    let U = membershipRandom(c, X)
     let P = 0
     let i = 0
 
     while (i < maxIter) {
-        // console.log('before', { V, U, P, i })
         let V = centroid(w, U, X)
         let P1 = objective(w, U, X, V)
         U = membership(w, X, V)
 
-        // console.log({ P1, i })
         if (P1 - P < err && P1 - P > 0) break
         P = P1
         i++
-        // console.log('after', { V, U, P, i })
     }
     console.log({ P, i })
 
